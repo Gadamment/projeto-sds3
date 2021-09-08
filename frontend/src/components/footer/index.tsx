ee645f3a73f786bf8c89f79e198faf5504fcab0a
Gadamment <josecarvalhobneto@gmail.com>
2021-09-08 03:56:07 -0300

    Basic static components

---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+        
+        <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por Gadamment<a href="https://github.com/Gadamment" target="_blank" rel="noreferrer">José Neto</a></p>
+    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+  </div>
+</footer>
+    );
+  }
+  
+  export default Footer;
+  
